c0aad5c43209b9cf640cbe650262a46cb86f1156
iti <italo.felipe.ignacio@hotmail.com>
2024-07-27 15:23:11 -0300

    first commit

---
+++ src/presentation/environment/splash/index.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from 'react-native';
+import type { FC } from 'react';
+
+export const SplashScreen: FC = () => (
+  <View className={'flex min-h-screen justify-center items-center bg-primary'}>
+    <Text className={''}>sasa</Text>
+  </View>
+);
